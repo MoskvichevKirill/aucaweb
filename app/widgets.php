bd5de412058885a1b17fcde3691d45fa1e1696b4
rem4ik4ever@gmail.com <rem4ik4ever@gmail.com>
2014-01-27 15:53:00 +0600

    header footer divided, login form and wodgets added

---
+++ app/widgets.php
@@ -0,0 +1,10 @@
+<div class="popup hidden">
+	<div class="login-form">
+		<form action="" id="logform" autocomplete="on">
+			<h3>Login</h3>
+			<div class="inp"><div class="title">Email:</div><div class="field"><input id="email" type="email" required></div></div>
+			<div class="inp"><div class="title">Password:</div><div class="field"><input id="passw" type="password" required></div></div>
+			<input id="login" class="btn" type="submit" value="Login">
+		</form>
+	</div>
+</div>
