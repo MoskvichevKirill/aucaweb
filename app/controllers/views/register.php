aaf0e16290c3126052d6b990fd9169a1f4e49601
Danyar <shamkeyev@mail.ru>
2014-02-03 10:59:47 +0600

    added registration form and styles

--- app/controllers/views/register.php
+++ app/controllers/views/register.php
@@ -1,1 +1,24 @@
-<h1>Register page</h1>
+<div class="signupform">
+	<h2>Hello. Sign up for our website. It's free.</h2>
+	<hr>
+	<form method="POST">
+		<div>Enter your preferred username</div>
+		<div class="field">
+			<input class="input" name="username" type="text" required/>
+		</div></br>
+		<div>Enter your email</div>
+		<div class="field">
+			<input name="email" type="email" required/>
+		</div></br>
+		<div>Enter your new password</div>
+		<div class="field">
+			<input name="password" type="password" required/>
+		</div></br>
+		<div>Retype password</div>
+		<div class="field">
+			<input name="password" type="password" required/>
+		</div>
+	 	<p style="margin-left:130px;"><input class="btn" type="submit" value="Submit"/></p>
+		
+	 </form>
+</div>
