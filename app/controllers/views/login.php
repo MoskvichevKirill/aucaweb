b1b58f8cc68c67e570de25011013628da853ae28
Aidai <b_aidai@mail.ru>
2014-02-26 09:18:59 +0600

    simple Login form

--- app/controllers/views/login.php
+++ app/controllers/views/login.php
@@ -1,1 +1,16 @@
-<h1>Login page hello</h1>
+<div class="loginfe">
+  <h1>Login to Q&A</h1>
+  <hr>
+	  <form action = "">
+	        <p><input type="text" name="login" value="" placeholder="Username or Email"></p>
+	        <p><input type="password" name="password" value="" placeholder="Password"></p>
+	        <p class="remember_me">
+	          <label>
+	            <input type="checkbox" name="remember_me" id="remember_me">
+	            Remember me on this computer
+	          </label>
+	        </p>
+	        <p class="submit"><input type="submit" name="commit" value="Login"></p>
+	   </form>
+</div>
+		    
