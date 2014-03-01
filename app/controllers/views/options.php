ced170c4eb62ae5f3b8d8a38dfc1b20054b42990
Aidai <b_aidai@mail.ru>
2014-03-01 16:05:06 +0600

    Personal settings, change password and e-mail

---
+++ app/controllers/views/options.php
@@ -0,0 +1,15 @@
+<div class="options">
+<meta charset="utf-8">
+	<h2>Личные настройки</h2>
+	<hr>
+	<form>
+		<input class="mybutton" value="Поменять пароль" name="change_password()">
+	</form>
+
+	<form>
+		<input class="mybutton" value="Поменять e-mail" name="change_mail()">
+	</form>
+
+</div>
+
+
