ed56197d7a608403e4ffe1cb9662bb7f35a65852
rem4ik4ever@gmail.com <rem4ik4ever@gmail.com>
2014-03-02 14:53:24 +0600

    fixed home view

--- app/controllers/views/home.php
+++ app/controllers/views/home.php
@@ -1,13 +1,15 @@
-<h1>Hello Home page</h1>
-
- <div class="containerM">
-	 		<div class="score"><ul>
-			 	<li class="a1">>  </li>
-			 	<li class="a">100  </li>
-			 	<li class="a1"><  </li>
-	 	</ul></div>
-	 	<div class="question">
-	 		<h2>Почему небо голубое?</h2>
-	 		<span> Потому-что так надо</span>
+<div class="questions">
+	<div class="question">
+		<div class="score">
+			<ul>
+			 	<li class="up"><div class="arrow">></div></li>
+			 	<li class="score_number"><div>100</div></li>
+			 	<li class="down"><div class="arrow"><</div></li>
+			</ul>
+		</div>
+			<div class="question_content">
+				<div class="question_title"><a href="#question">Почему небо голубое?</a></div>
+				<div class="question_desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo, cumque, et, esse, nihil minus corporis ratione dolores sint quos minima pariatur ab possimus harum voluptatem ad quo sequi ullam illo!</div>
 		</div>
 	</div>
+</div>
